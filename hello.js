7852c6d1f24405b7cadc1c6bbbac7f6f4e4609fb
Devil <hemanth040304@gmail.com>
2025-05-27 18:19:27 +0530

    resolving merge conflict while adding my code to the bug fix code

--- hello.js
+++ hello.js
@@ -1,4 +1,12 @@
 console.log("Hello, World!");
 console.log("Hello, gitHUb");
 
+
 //  Implement Urgent fix
+
+// I'm in the process of implementing a new feature
+// Code
+// Code
+// Code
+// Code
+
